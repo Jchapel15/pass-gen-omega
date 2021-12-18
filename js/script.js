e7c42e9ff36b5cdddb7101cc9c099ced0722d5f6
Jon Chapel <jchapel15@gmail.com>
2021-12-18 10:20:10 -0500

    added pass length verification, starting character prompts

--- js/script.js
+++ js/script.js
@@ -3,14 +3,42 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789"
+const symbols = "!@#$%^&*()" 
 
+//added userSelection & finalPassword var
+var userSelection = [];
+var finalPassword = [];
+
+ //prompt user for password length
+function generatePassword() {
   console.log("Button is clicked");
-  //prompt user for password criteria
-  //password length between 8-128
+ var passLength = window.prompt("How long would you like your password to be? (8-128 characters)")
+
+ //Pass Length Validation
+ if (passLength <8) {
+   alert ("Please insert a number between 8 to 128");
+   generatePassword();
+ }
+ if (passLength >128) {
+  alert ("Please insert a number between 8 to 128");
+  generatePassword;
+}
+
+var upperC = confirm("Would you like Uppercase Letters");
+if (upperC) {
+  userSelection = userSelection.
+}
+
+  //prompt user for password criteria 
+  //password length between 8-128 ****
   //Lowercase, Uppercase, numbers, special characters
   //Validate the input
+  //Generate password
   //Display generated password
+
 
   return "Generated password will go here";
 }
@@ -23,6 +51,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
