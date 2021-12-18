3de383fab7c5777353b8f38b8c7fe64e00177308
Jon Chapel <jchapel15@gmail.com>
2021-12-18 14:23:34 -0500

    implemented new methodology for way site was to be structured.  Refactor, kept code dry as much as possible.  Added comments in to explain functionality of sections

--- js/script.js
+++ js/script.js
@@ -1,77 +1,43 @@
-// Assignment code here
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789"
-const specialChar = "!@#$%^&*()" 
-
-//added userSelection & finalPassword var
-var userSelection = [];
-var finalPassword = [];
-
- //prompt user for password length
 function generatePassword() {
-  console.log("Button is clicked");
- var passLength = window.prompt("How long would you like your password to be? (8-128 characters)")
 
- //Pass Length Validation
- if (passLength <8) {
-   alert ("Please insert a number between 8 to 128");
-   generatePassword();
- }
- if (passLength >128) {
-  alert ("Please insert a number between 8 to 128");
-  generatePassword;
-}
-
-var upperC = confirm("Would you like Uppercase Letters");
-if (upperC) {
-  userSelection = userSelection.push(upperCase);
-}
-
-var lowerC = confirm("Would you like Lowercase Letters");
-if (lowerC) {
-  userSelection = userSelection.push(lowerCase)
-}
-
-//added remaining specChar and numb var
-var specC = confirm("Would you like and Special Characters");
-if(specC) {
-  userSelection.push(specialChar);
-}
-
-var numb = confirm("Would you like to add Numbers to your password?"); 
-if(numb) { 
-  userSelection.push(numbers);
-}
-
-if (userSelection.length ===0);
-alert("Not enough criteria selected to generate password.")
-generatePassword();
-
-console.log("user selection = " + userSelection);
-
-
-
-  //prompt user for password criteria ****
-  //password length between 8-128 ****
-  //Lowercase, Uppercase, numbers, special characters ****
-  //Validate the input ****
-  //Generate password
-  //Display generated password
-  return "Generated password will go here";
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var length = Number(prompt("How many characters would you like your password to be?"));
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+  
+  var uppers = confirm("Would you like to use uppercase letters?");
+  var lowers = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
+  
+  while (!uppers && !lowers && !numbers && !symbols) {
+    alert("You must select at least one character type!");
+    uppers = confirm("Would you like to use uppercase letters?");
+    lowers = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confirm("Would you like to use special characters?");
+  }
+  
+    var password = "";
+  
+    var allowed = {};
+    if (uppers) password += rando(allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
+    if (lowers) password += rando(allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
+    if (numbers) password += rando(allowed.numbers = "1234567890");
+    if (symbols) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
+  
+    for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
+  
+    return password;
+  }
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
